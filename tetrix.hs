bb496fa00ec001e87dcec336447c9808e1e183fd
e.e d3si9n <eed3si9n@gmail.com>
2010-09-18 16:21:48 -0400

    implemented random blocks.

--- tetrix.hs
+++ tetrix.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import System.Random
 import qualified Data.Map as Map
 import Data.Maybe
 import Control.Exception
@@ -9,12 +10,14 @@ import qualified UI.HSCurses.Curses as C
 defaultBoardWidth = 9
 defaultBoardHeight = 20
 origin = (0, 0)
+seed = 0 :: Int
 
-data Cell = CTee
+data Cell = Tee | Bar | Box | El | Jay | Es | Zee
 
 data Block = Block {
   blockPos :: (Int, Int),
-  blockLocals :: [(Double, Double)]
+  blockLocals :: [(Double, Double)],
+  blockKind :: Cell
 }
 
 type CellMap = Map.Map (Int, Int) Cell
@@ -26,7 +29,8 @@ data Board = Board {
 
 data TetrixVar = TetrixVar {
   gameBlock :: Block,
-  gameBoard :: Board
+  gameBoard :: Board,
+  gameGen :: StdGen
 }
 
 type TetrixState = StateT TetrixVar IO
@@ -40,43 +44,91 @@ initBoard width height =
       boardCells = Map.empty
     }
     
-initBlock board =
+initBlock board kind =
     Block {
       blockPos = (fst s `div` 2, snd s - 2),
-      blockLocals = [(0.0, 0.0), (-1.0, 0.0), (1.0, 0.0), (0.0, 1.0)]
+      blockLocals = initLocals kind,
+      blockKind = kind
     }
     where s = boardSize board
 
+initLocals kind =
+    case kind of
+      Tee -> [(0.0, 0.0), (-1.0, 0.0), (1.0, 0.0), (0.0, 1.0)]
+      Bar -> [(0.0, -1.5), (0.0, -0.5), (0.0, 0.5), (0.0, 1.5)]
+      Box -> [(-0.5, 0.5), (0.5, 0.5), (-0.5, -0.5), (0.5, -0.5)]
+      El  -> [(0.0, 0.0), (0.0, 1.0), (0.0, -1.0), (1.0, -1.0)]
+      Jay -> [(0.0, 0.0), (0.0, 1.0), (0.0, -1.0), (-1.0, -1.0)]
+      Es  -> [(-0.5, 0.0), (0.5, 0.0), (-0.5, 1.0), (0.5, -1.0)]
+      Zee -> [(-0.5, 0.0), (0.5, 0.0), (-0.5, -1.0), (0.5, 1.0)]
+
 initState =
     TetrixVar {
-      gameBlock = initBlock board,
-      gameBoard = board
+      gameBlock = block,
+      gameBoard = board,
+      gameGen = gen'
     }
     where board = initBoard defaultBoardWidth defaultBoardHeight
+          (kind, gen') = randomKind (mkStdGen seed)
+          block = initBlock board kind
+
+randomKind gen =
+    (head $ drop value cells, gen')
+    where (value, gen') = randomR (0, length cells - 1) gen
+          cells = [Tee, Bar, Box, El, Jay, Es, Zee]
+          
+randomKindTransition var =
+    (kind,
+    TetrixVar {
+      gameBlock = gameBlock var,
+      gameBoard = gameBoard var,
+      gameGen = gen'
+    })
+    where (kind, gen') = randomKind (gameGen var)
 
 blockCells :: Block -> [(Int, Int)]
 blockCells block =
-    map (\x -> roundPair x `addPair` blockPos block) $ blockLocals block
+    map (\x -> floorPair x `addPair` blockPos block) $ blockLocals block
 
 blockMoveBy :: (Int, Int) -> Block -> Block
 blockMoveBy delta block =
     Block {
       blockPos = delta `addPair` blockPos block,
-      blockLocals = blockLocals block
+      blockLocals = blockLocals block,
+      blockKind = blockKind block
     }
     
 left = blockMoveBy (-1, 0)
 right = blockMoveBy (1, 0)
 down = blockMoveBy (0, -1)
 
+blockRotate :: Double -> Block -> Block
+blockRotate theta block =
+    Block {
+      blockPos = blockPos block,
+      blockLocals = map (\x -> roundHalfPair (rotatePair x theta)) locals,
+      blockKind = blockKind block
+    }
+    where locals = blockLocals block
+          roundHalfPair p = (roundHalf $ fst p, roundHalf $ snd p)
+          roundHalf x = 0.5 * (fromIntegral $ round $ 2.0 * x)
+
+clockwise = blockRotate $ (-pi) / 2
+    
 addPair lhs rhs = (fst lhs + fst rhs, snd lhs + snd rhs)
 
 gePair lhs rhs = fst lhs >= fst rhs && snd lhs >= snd rhs 
 
 ltPair lhs rhs = fst lhs < fst rhs && snd lhs < snd rhs
 
-roundPair :: (Double, Double) -> (Int, Int)
-roundPair p = (round $ fst p, round $ snd p)
+floorPair :: (Double, Double) -> (Int, Int)
+floorPair p = (floor $ fst p, floor $ snd p)
+
+rotatePair :: (Double, Double) -> Double -> (Double, Double)
+rotatePair p theta =
+    (c * fst p - s * snd p, s * fst p - c * snd p)
+    where c = cos theta
+          s = sin theta        
 
 boolToMaybe :: a -> Bool -> Maybe a
 boolToMaybe x b =
@@ -84,13 +136,9 @@ boolToMaybe x b =
     then Just x
     else Nothing
 
-load block =
-    mapBlockCells step block
-    where step mp x = Map.insert x CTee mp
+load block = mapBlockCells (\mp x -> Map.insert x (blockKind block) mp) block
 
-unload block =
-    mapBlockCells step block
-    where step mp x = Map.delete x mp
+unload block = mapBlockCells (\mp x -> Map.delete x mp) block
 
 mapBlockCells :: (CellMap -> (Int, Int) -> CellMap) -> Block -> Board -> Board
 mapBlockCells step block board =
@@ -112,7 +160,8 @@ loadBlockOrNot block board var =
     block `notCollidesM` board >>= \_ ->
     Just TetrixVar {
       gameBlock = block,
-      gameBoard = load block board
+      gameBoard = load block board,
+      gameGen = gameGen var
     }
     where notCollidesM b d = boolToMaybe b (not $ b `collides` d)
           inBoundM b d = boolToMaybe b $ b `inBound` d 
@@ -129,9 +178,10 @@ tick var =
     where hitTheFloor = fromMaybe var $ loadNewBlock var
     
 loadNewBlock var =
-    loadBlockOrNot block' board var
-    where block' = initBlock board
+    loadBlockOrNot block' board var'
+    where block' = initBlock board kind
           board = gameBoard var
+          (kind, var') = randomKindTransition var
 
 transformOrNot f var = fromMaybe var $ transform f var
 
@@ -152,9 +202,10 @@ drawTetrix w var =
           pos = blockPos $ gameBlock var
     
 drawBoard w board =
-    do mapM_ (\pos ->
-         C.mvWAddStr w (defaultBoardHeight - snd pos) (fst pos) "*") $ Map.keys $ boardCells board
-
+    do mapM_ drawCell cells
+    where cells = Map.keys $ boardCells board
+          drawCell pos = C.mvWAddStr w (defaultBoardHeight - snd pos) (fst pos) "*"
+    
 redraw w =
    do liftIO $ C.wclear w
       var <- get
@@ -176,6 +227,7 @@ eventloop w frame =
             do k <- liftIO C.getch
                case C.decodeKey k of
                  C.KeyChar 'q'  -> return ()
+                 C.KeyUp        -> process $ transformOrNotM w clockwise
                  C.KeyLeft      -> process $ transformOrNotM w left
                  C.KeyRight     -> process $ transformOrNotM w right
                  C.KeyDown      -> process $ transitionM w tick
